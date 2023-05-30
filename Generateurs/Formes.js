8b563ec6958967a1db2229b37f777cb4cfd9a75d
VSProgramme <38361226+VincentSinel@users.noreply.github.com>
2023-05-30 19:40:48 +0200

    Suppression raphael exporter (ne gèrant pas certain type de tracé)

--- Generateurs/Formes.js
+++ Generateurs/Formes.js
@@ -953,30 +953,6 @@ function Save()
 	blob = new Blob([svgString], {"type": "image/svg+xml"});
 	a.href = (window.URL || webkitURL).createObjectURL(blob);
 	a.click();
-	return;
-	// var svg = paper.toSVG();
-	// navigator.clipboard.writeText(svg);
-
-	// Alert the copied text
-	// alert("SVG copied");
-	
-	// let filename = "Forme.svg"
-	// let type = ".svg"
-	// var file = new Blob([svg], {type: type});
-	// if (window.navigator.msSaveOrOpenBlob) // IE10+
-	// 	window.navigator.msSaveOrOpenBlob(file, filename);
-	// else { // Others
-	// 	var a = document.createElement("a"),
-	// 			url = URL.createObjectURL(file);
-	// 	a.href = url;
-	// 	a.download = filename;
-	// 	document.body.appendChild(a);
-	// 	a.click();
-	// 	setTimeout(function() {
-	// 		document.body.removeChild(a);
-	// 		window.URL.revokeObjectURL(url);  
-	// 	}, 0); 
-	// }
 }
 
 function SavePNG() {
