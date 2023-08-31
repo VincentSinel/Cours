70283c565248807a15bf757a0dcef22fe13cd210
VSProgramme <38361226+VincentSinel@users.noreply.github.com>
2023-08-31 09:20:07 +0200

    Ajout indication chapitre non terminé

--- JS/Cours/Liste.js
+++ JS/Cours/Liste.js
@@ -4,101 +4,121 @@ var ListeCours = {
             id: 1,
             nom: "Fractions irréductibles",
             icone: 1,
+            ready : true,
         },
         {
             id: 2,
             nom: "Homothéties",
             icone: 2,
+            ready : false,
         },
         {
             id: 3,
             nom: "Puissances",
             icone: 1,
+            ready : false,
         },
         {
             id: 4,
             nom: "Fonctions (Généralités)",
             icone: 3,
+            ready : false,
         },
         {
             id: 5,
             nom: "Transformations",
             icone: 2,
+            ready : false,
         },
         {
             id: 6,
             nom: "Factorisations développements",
             icone: 1,
+            ready : false,
         },
         {
             id: 7,
             nom: "Gestions de données",
             icone: 5,
+            ready : false,
         },
         {
             id: 8,
             nom: "Théorème de Thalès Sens Direct",
             icone: 2,
+            ready : false,
         },
         {
             id: 9,
             nom: "Equations",
             icone: 1,
+            ready : false,
         },
         {
             id: 10,
             nom: "Trigonométrie",
             icone: 2,
+            ready : false,
         },
         {
             id: 11,
             nom: "Médiane et étendu",
             icone: 5,
+            ready : false,
         },
         {
             id: 12,
             nom: "Fonctions linéaires et affines",
             icone: 3,
+            ready : false,
         },
         {
             id: 13,
             nom: "Volumes (Boules)",
             icone: 2,
+            ready : false,
         },
         {
             id: 14,
             nom: "Trigonométrie Calcul d'angle",
             icone: 2,
+            ready : false,
         },
         {
             id: 15,
             nom: "Reciproque de Thalès",
             icone: 2,
+            ready : false,
         },
         {
             id: 16,
             nom: "Equations produit nul",
             icone: 1,
+            ready : false,
         },
         {
             id: 17,
             nom: "Fonction affines",
             icone: 3,
+            ready : false,
         },
         {
             id: 18,
             nom: "Probabilités",
             icone: 4,
+            ready : false,
         },
         {
             id: 19,
             nom: "Ratios",
             icone: 1,
+            ready : false,
         },
         {
             id: 20,
             nom: "Repérage dans l'espace",
             icone: 2,
+            ready : false,
         }
     ],
     quatrieme: [
@@ -106,106 +126,127 @@ var ListeCours = {
             id: 1,
             nom: "Nombres premiers",
             icone: 1,
+            ready : true,
         },
         {
             id: 2,
             nom: "Théorème de Pythagore",
             icone: 2,
+            ready : true,
         },
         {
             id: 3,
             nom: "Opérations sur les relatifs",
             icone: 1,
+            ready : true,
         },
         {
             id: 4,
             nom: "Translations et pavages",
             icone: 2,
+            ready : true,
         },
         {
             id: 5,
             nom: "Additions et soustractions de fractions",
             icone: 1,
+            ready : true,
         },
         {
             id: 6,
             nom: "Egalité des produits en croix",
             icone: 5,
+            ready : true,
         },
         {
             id: 7,
             nom: "Rotations",
             icone: 2,
+            ready : true,
         },
         {
             id: 8,
             nom: "Puissances positives",
             icone: 1,
+            ready : true,
         },
         {
             id: 9,
             nom: "Distributivité simple",
             icone: 3,
+            ready : true,
         },
         {
             id: 10,
             nom: "Coordonnées sphériques",
             icone: 2,
+            ready : true,
         },
         {
             id: 11,
             nom: "Réciproque du théorème de Pythagore",
             icone: 2,
+            ready : true,
         },
         {
             id: 12,
             nom: "Multiplications et divisions de fractions",
             icone: 1,
+            ready : true,
         },
         {
             id: 13,
             nom: "Equation simple",
             icone: 3,
+            ready : true,
         },
         {
             id: 14,
             nom: "Puissances négatives",
             icone: 1,
+            ready : true,
         },
         {
             id: 15,
             nom: "Triangle semblable",
             icone: 2,
+            ready : true,
         },
         {
             id: 16,
             nom: "Pyramides et cônes",
             icone: 2,
+            ready : true,
         },
         {
             id: 17,
             nom: "Médiane et étendue",
             icone: 5,
+            ready : true,
         },
         {
             id: 18,
             nom: "Proportionnalité",
             icone: 5,
+            ready : true,
         },
         {
             id: 19,
             nom: "Probabilité",
             icone: 4,
+            ready : true,
         },
         {
             id: 20,
             nom: "Distributivité double",
             icone: 3,
+            ready : true,
         },
         {
             id: 21,
             nom: "Cosinus",
             icone: 3,
+            ready : true,
         }
     ],
     cinquieme: [
@@ -213,101 +254,121 @@ var ListeCours = {
             id: 1,
             nom: "Priorités opératoires",
             icone: 1,
+            ready : false,
         },
         {
             id: 2,
             nom: "Symétries centrale",
             icone: 2,
+            ready : false,
         },
         {
             id: 3,
             nom: "Nombres Relatifs",
             icone: 1,
+            ready : false,
         },
         {
             id: 4,
             nom: "Algorithme",
             icone: 2,
+            ready : false,
         },
         {
             id: 5,
             nom: "Introduction au calcul littéral",
             icone: 1,
+            ready : false,
         },
         {
             id: 6,
             nom: "Triangles - Partie 1",
             icone: 5,
+            ready : false,
         },
         {
             id: 7,
             nom: "Statistiques",
             icone: 2,
+            ready : false,
         },
         {
             id: 8,
             nom: "Nombres Premiers",
             icone: 1,
+            ready : false,
         },
         {
             id: 9,
             nom: "Fractions et Proportions",
             icone: 3,
+            ready : false,
         },
         {
             id: 10,
             nom: "Tableaux de Proportionnalité",
             icone: 2,
+            ready : false,
         },
         {
             id: 11,
             nom: "Additions et Soustractions de Nombres Relatifs",
             icone: 2,
+            ready : false,
         },
         {
             id: 12,
             nom: "Triangles - Partie 2",
             icone: 1,
+            ready : false,
         },
         {
             id: 13,
             nom: "Probabilités",
             icone: 3,
+            ready : false,
         },
         {
             id: 14,
             nom: "Fraction, Somme et Différence",
             icone: 1,
+            ready : false,
         },
         {
             id: 15,
             nom: "Egalité et Réduction d'Expressions Littérales",
             icone: 2,
+            ready : false,
         },
         {
             id: 16,
             nom: "Aires",
             icone: 2,
+            ready : false,
         },
         {
             id: 17,
             nom: "Angles",
             icone: 5,
+            ready : false,
         },
         {
             id: 18,
             nom: "Proportionnalité, Pourcentage et Produit en Croix",
             icone: 5,
+            ready : false,
         },
         {
             id: 19,
             nom: "Solide",
             icone: 4,
+            ready : false,
         },
         {
             id: 20,
             nom: "Parallélogrammes",
             icone: 3,
+            ready : false,
         }
     ],
     sixieme: [
@@ -315,106 +376,127 @@ var ListeCours = {
             id: 1,
             nom: "Nombres premiers",
             icone: 1,
+            ready : false,
         },
         {
             id: 2,
             nom: "Théorème de Pythagore",
             icone: 2,
+            ready : false,
         },
         {
             id: 3,
             nom: "Opérations sur les relatifs",
             icone: 1,
+            ready : false,
         },
         {
             id: 4,
             nom: "Translations et pavages",
             icone: 2,
+            ready : false,
         },
         {
             id: 5,
             nom: "Additions et soustractions de fractions",
             icone: 1,
+            ready : false,
         },
         {
             id: 6,
             nom: "Egalité des produits en croix",
             icone: 5,
+            ready : false,
         },
         {
             id: 7,
             nom: "Rotations",
             icone: 2,
+            ready : false,
         },
         {
             id: 8,
             nom: "Puissances positives",
             icone: 1,
+            ready : false,
         },
         {
             id: 9,
             nom: "Distributivité simple",
             icone: 3,
+            ready : false,
         },
         {
             id: 10,
             nom: "Coordonnées sphériques",
             icone: 2,
+            ready : false,
         },
         {
             id: 11,
             nom: "Réciproque du théorème de Pythagore",
             icone: 2,
+            ready : false,
         },
         {
             id: 12,
             nom: "Multiplication et division de fraction",
             icone: 1,
+            ready : false,
         },
         {
             id: 13,
             nom: "Equation simple",
             icone: 3,
+            ready : false,
         },
         {
             id: 14,
             nom: "Puissances avec exposants négatifs",
             icone: 1,
+            ready : false,
         },
         {
             id: 15,
             nom: "Triangle semblable",
             icone: 2,
+            ready : false,
         },
         {
             id: 16,
             nom: "Pyramides et cônes",
             icone: 2,
+            ready : false,
         },
         {
             id: 17,
             nom: "Médiane et étendue",
             icone: 5,
+            ready : false,
         },
         {
             id: 18,
             nom: "Proportionnalité",
             icone: 5,
+            ready : false,
         },
         {
             id: 19,
             nom: "Probabilité",
             icone: 4,
+            ready : false,
         },
         {
             id: 20,
             nom: "Distributivité double",
             icone: 3,
+            ready : false,
         },
         {
             id: 21,
             nom: "Cosinus",
             icone: 3,
+            ready : false,
         }
     ],
 }
@@ -479,10 +561,13 @@ function CreateDiv(parent, name, folder)
         const element = ListeCours[name][i];
         var a = document.createElement("div");
         a.className += "chapitrebutton"
-        a.onclick = function() { 
-            clic('Cours/' + folder + 'eme/Chapitre_' + element.id + '/Cours.html', this); }
-        //a.ontouchend = function() {
-        //    clic('Cours/' + folder + 'eme/Chapitre_' + element.id + '/Cours.html'); }
+        if (element.ready)
+        {
+            a.onclick = function() { 
+                clic('Cours/' + folder + 'eme/Chapitre_' + element.id + '/Cours.html', this); }
+            //a.ontouchend = function() {
+            //    clic('Cours/' + folder + 'eme/Chapitre_' + element.id + '/Cours.html'); }
+        }
         
         var b = document.createElement("div");
         b.className += "chapitrebutttonflex"
@@ -494,6 +579,10 @@ function CreateDiv(parent, name, folder)
         var d = document.createElement("div");
         var e = document.createElement("p");
         e.className += "chapitrebutttonnumber"
+        if (!element.ready)
+        {
+            e.className += " strike"
+        }
         e.innerHTML = "Chapitre " + element.id;
         var f = document.createElement("p");
         f.className += "chapitrebutttonname"
