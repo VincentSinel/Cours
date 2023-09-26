3514957159479aa1f8c657ef40d32826b0b61442
VSProgramme <38361226+VincentSinel@users.noreply.github.com>
2023-09-26 19:01:00 +0200

    UpBoulier

--- Test/Boulier.js
+++ Test/Boulier.js
@@ -1,19 +1,20 @@
-window.addEventListener("load", Generate);
+const TCOLONNE = 12
 
-const TCOLONNE = 9
+window.addEventListener("load", () => {Generate(TCOLONNE)});
 const BOULE = '<svg viewBox="0 0 100 100" class="boule"><circle cx="50" cy="50" r="49" fill="currentColor" stroke="#442C40" stroke-width="2"/></svg><div class="empty"></div>'
-// Esri color ramps - Circling Circus
-// rgba(217, 43, 48, 1),rgba(0, 149, 186, 1),rgba(60, 204, 180, 1),rgba(171, 82, 179, 1),rgba(255, 178, 89, 1),rgba(255, 223, 60, 1),rgba(235, 130, 235, 1),rgba(194, 124, 48, 1),rgba(160, 209, 125, 1),rgba(242, 96, 161, 1)
-const COLORS = ["rgba(217, 43, 48, 0.5)", "rgba(0, 149, 186, 0.5)", "rgba(60, 204, 180, 0.5)", "rgba(171, 82, 179, 0.5)", "rgba(255, 178, 89, 0.5)", "rgba(255, 223, 60, 0.5)", "rgba(235, 130, 235, 0.5)", "rgba(194, 124, 48, 0.5)", "rgba(160, 209, 125, 0.5)", "rgba(242, 96, 161, 0.5)"];
 
-function Generate()
+// Esri color ramps - Metro Movement
+// rgba(237, 81, 81, 1),rgba(20, 158, 206, 1),rgba(167, 198, 54, 1),rgba(158, 85, 156, 1),rgba(252, 146, 31, 1),rgba(255, 222, 62, 1),rgba(247, 137, 216, 1),rgba(183, 129, 74, 1),rgba(60, 175, 153, 1),rgba(107, 107, 214, 1),rgba(181, 71, 121, 1),rgba(127, 127, 127, 1)
+const COLORS = ["rgba(237, 81, 81, 0.5)", "rgba(20, 158, 206, 0.5)", "rgba(167, 198, 54, 0.5)", "rgba(158, 85, 156, 0.5)", "rgba(252, 146, 31, 0.5)", "rgba(255, 222, 62, 0.5)", "rgba(247, 137, 216, 0.5)", "rgba(183, 129, 74, 0.5)", "rgba(60, 175, 153, 0.5)", "rgba(107, 107, 214, 0.5)", "rgba(181, 71, 121, 0.5)", "rgba(127, 127, 127, 0.5)"];
+
+function Generate(tcolonne)
 {
     let l1 = document.getElementById("line1");
     let l2 = document.getElementById("line2");
     l1.innerHTML = "";
     l2.innerHTML = "";
     let d; let t;
-    for (let i = 0; i < TCOLONNE; i++) {
+    for (let i = 0; i < tcolonne; i++) {
         //Create first line.
         d = document.createElement("div");
         d.classList.add("colonne");
