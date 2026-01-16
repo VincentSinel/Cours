9130a18fef74f901d02b405caebe2108d5178327
VSProgramme <vincentsinel@gmail.com>
2026-01-16 12:49:43 +0100

    Ajout chapitre 10-2 6ème

--- Cours/Liste_Chapitre.js
+++ Cours/Liste_Chapitre.js
@@ -2652,7 +2652,7 @@ var sixieme_saintexupery = [
 			actx: 		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 10 - Activité d'introduction.docx",
 			act:  		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 10 - Activité d'introduction.pdf",
 		},
-		ready : false,
+		ready : true,
 		page : "SaintExupery/Chapitre_10_2/Cours.html",
 	}, // Chapitre 10 Partie 2
 	{
