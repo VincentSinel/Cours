2405fb6820bb92cceeb0dc1368bfdaf02c679964
VSProgramme <vincentsinel@gmail.com>
2026-01-16 12:45:27 +0100

    Ajout chapitre 12-3 6ème

--- Cours/Liste_Chapitre.js
+++ Cours/Liste_Chapitre.js
@@ -2748,7 +2748,7 @@ var sixieme_saintexupery = [
 			actx: 		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 12 - Activité d'introduction.docx",
 			act:  		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 12 - Activité d'introduction.pdf",
 		},
-		ready : false,
+		ready : true,
 		page : "SaintExupery/Chapitre_12_3/Cours.html",
 	}, // Chapitre 12 Partie 3
 	{
