83c038dae27bdfe8986028f6298fe2848865b452
VSProgramme <vincentsinel@gmail.com>
2026-01-26 08:44:16 +0100

    Ajout base exerciseur

--- Exerciseur/Exercices/Generator_1.js
+++ Exerciseur/Exercices/Generator_1.js
@@ -1,10 +1,172 @@
-// Exemple d'un générateur d'exercice
+// Figure avec droite demi-droite et segments.
+// 5 points A, B, C, D et E.
+// E non nommé
+// Cite deux droites sécantes.
+// Nommer E le point d'intersection de $(d_1)$ et $(d_2)$.
+// Que peut-on dire des points B, D et C ?.
+// Repasser en bleu [BD].
+// Repasser en vert [AC).
+
+function CreateObjects() {
+
+	let A = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	let B = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	while (A.distanceTo(B) < 50) {
+		B = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	}
+	let D = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	while (D.distanceTo(A) < 50 || D.distanceTo(B) < 50) {
+		D = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	}
+	let F = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	while (F.distanceTo(A) < 50 || F.distanceTo(B) < 50 || F.distanceTo(D) < 50) {
+		F = new Vecteur2(50 + Math.round(Math.random() * 300), 50 + Math.round(Math.random() * 300));
+	}
+
+	let AD = new Ligne(A, D);
+	let temp = AD.toBorder(400, 400)
+	let E = temp.pointIn(0.2 + 0.6 * Math.random());
+	while (E.distanceTo(A) < 20 || E.distanceTo(D) < 20 || E.distanceTo(B) < 20) {
+		E = temp.pointIn(0.2 + 0.6 * Math.random());
+	}
+	let BD = new Ligne(B, D);
+	let BE = new Ligne(B, E);
+	let AF = new Ligne(A, F);
+
+	temp = BD.toBorder(400, 400)
+	let C = temp.pointIn(0.2 + 0.6 * Math.random());
+	while (C.distanceTo(A) < 20 || C.distanceTo(D) < 20 || C.distanceTo(B) < 20) {
+		C = temp.pointIn(0.2 + 0.6 * Math.random());
+	}
+	let AC = new Ligne(A, C);
+	
+	let objects = {
+		"points": [A, B, C, D, E, F],
+		"points_position": [180 * (1 - Math.random() * 2.0), 180 * (1 - Math.random() * 2.0), 180 * (1 - Math.random() * 2.0), 180 * (1 - Math.random() * 2.0)],
+		"lines": [AD, BD, BE, AF, AC, ]
+	}
+
+	return objects;
+}
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
+
+
+function CreateFigure(point_name, line_name, objects) 
+{
+	let figure = new Geometrie(400,400);
+
+	figure.AjouterPoint(objects["points"][0], point_name[0], {"angle": objects["points_position"][0]});
+	figure.AjouterPoint(objects["points"][1], point_name[1], {"angle": objects["points_position"][1]});
+	figure.AjouterPoint(objects["points"][2], point_name[2], {"angle": objects["points_position"][2]});
+	figure.AjouterPoint(objects["points"][3], point_name[3], {"angle": objects["points_position"][3]});
+
+	figure.AjouterDroite(objects["lines"][0], {nom:line_name[0], "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][1], {nom:line_name[2], "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][2], {nom:line_name[1], "nom-position": 1, "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][3], {nom:line_name[3], "nom-position": 1, "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDemiDroite(objects["lines"][4]);
+	return figure
+}
+
+function CreateFigure_correction(point_name, line_name, objects)
+{
+	let figure = new Geometrie(400,400);
+
+	figure.AjouterPoint(objects["points"][0], point_name[0], {"angle": objects["points_position"][0]});
+	figure.AjouterPoint(objects["points"][1], point_name[1], {"angle": objects["points_position"][1]});
+	figure.AjouterPoint(objects["points"][2], point_name[2], {"angle": objects["points_position"][2]});
+	figure.AjouterPoint(objects["points"][3], point_name[3], {"angle": objects["points_position"][3]});
+	figure.AjouterPoint(objects["points"][4], point_name[4], {"angle": 180 * (1 - Math.random() * 2.0), "attr": {"stroke": "red"}, "attr-text": {"fill": "red"}});
+
+	figure.AjouterDroite(objects["lines"][0], {nom:line_name[0], "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][1], {nom:line_name[2], "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][2], {nom:line_name[1], "nom-position": 1, "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDroite(objects["lines"][3], {nom:line_name[3], "nom-position": 1, "nom-attr": {attr:{"font-style": "italic"}}})
+	figure.AjouterDemiDroite(objects["lines"][4], {"attr": {"stroke": "lime", "stroke-width": 4}});
+	figure.AjouterSegment(objects["lines"][1], {"attr": {"stroke": "blue", "stroke-width": 4}});
+	return figure
+}
+
 
 
 generators.push((classe) => {
-	var exercice = document.createElement("div");
-	exercice.innerHTML = "exercice 1"
-	var correction = document.createElement("div");
+
+	
+	let point_name = ["A", "B", "C", "D", "E"];
+	let line_name = ["(d1)", "(d2)", "(d3)", "(d4)"];
+	shuffleArray(point_name);
+	shuffleArray(line_name);
+
+	let objects = CreateObjects();
+	{
+		let fig = CreateFigure(point_name, line_name, objects);
+		var exercice = document.createElement("div");
+		let content = document.createElement("div");
+		content.classList.add("flexobject")
+		let txt = document.createElement("div");
+		txt.classList.add("flexstretch")
+		let ol = document.createElement("ol");
+		let li1 = document.createElement("li");
+		li1.innerHTML = `Citer deux droites sécantes.`;
+		let li2 = document.createElement("li");
+		li2.innerHTML = `Nommer ${point_name[4]} le point d'intersection de $${line_name[0]}$ et $${line_name[1]}$.`;
+		let li3 = document.createElement("li");
+		li3.innerHTML = `Que peut-on dire des points ${point_name[1]}, ${point_name[2]} et ${point_name[3]} ?`;
+		let li4 = document.createElement("li");
+		li4.innerHTML = `Repasser en bleu le segment $[${point_name[1]}${point_name[3]}]$.`;
+		let li5 = document.createElement("li");
+		li5.innerHTML = `Repasser en vert la demi-droite $[${point_name[0]}${point_name[2]})$.`;
+		ol.appendChild(li1);
+		ol.appendChild(li2);
+		ol.appendChild(li3);
+		ol.appendChild(li4);
+		ol.appendChild(li5);
+		txt.appendChild(ol);
+		content.appendChild(fig.GetElement());
+		content.appendChild(txt);
+		exercice.appendChild(content);
+	}
+
+	{
+		let fig_correction = CreateFigure_correction(point_name, line_name, objects);
+		var correction = document.createElement("div");
+		let corr_content = document.createElement("div");
+		corr_content.classList.add("flexobject")
+		let corr_txt = document.createElement("div");
+		corr_txt.classList.add("flexstretch")
+		let corr_ol = document.createElement("ol");
+		let li1 = document.createElement("li");
+		li1.innerHTML = `Citer deux droites sécantes.`; li1.appendChild(document.createElement("br"));
+		let co1 = document.createElement("span"); co1.classList.add("correction");
+		co1.innerHTML = `Par exemple $${line_name[0]}$ et $${line_name[1]}$.`;
+		li1.appendChild(co1);
+		let li2 = document.createElement("li");
+		li2.innerHTML = `Nommer ${point_name[4]} le point d'intersection de $${line_name[0]}$ et $${line_name[1]}$.`;
+		let li3 = document.createElement("li");
+		li3.innerHTML = `Que peut-on dire des points ${point_name[1]}, ${point_name[2]} et ${point_name[3]} ?`; li3.appendChild(document.createElement("br"));
+		let co3 = document.createElement("span"); co3.classList.add("correction");
+		co3.innerHTML = `Les points ${point_name[1]}, ${point_name[2]} et ${point_name[3]} sont <b>alignés</b>.`;
+		li3.appendChild(co3);
+		let li4 = document.createElement("li");
+		li4.innerHTML = `Repasser en bleu le segment $[${point_name[1]}${point_name[3]}]$.`;
+		let li5 = document.createElement("li");
+		li5.innerHTML = `Repasser en vert la demi-droite $[${point_name[0]}${point_name[2]})$.`;
+		corr_ol.appendChild(li1);
+		corr_ol.appendChild(li2);
+		corr_ol.appendChild(li3);
+		corr_ol.appendChild(li4);
+		corr_ol.appendChild(li5);
+		corr_txt.appendChild(corr_ol);
+		corr_content.appendChild(fig_correction.GetElement());
+		corr_content.appendChild(corr_txt);
+		correction.appendChild(corr_content);
+	}
 
 	return { exercice: exercice, correction: correction };
 })
---
+++ Exerciseur/Geometrie.js
@@ -0,0 +1,527 @@
+class Vecteur2
+{
+	x;
+	y;
+	
+	constructor(x,y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	add(v)
+	{
+		return new Vecteur2(this.x + v.x, this.y + v.y);
+	}
+	sub(v)
+	{
+		return new Vecteur2(this.x - v.x, this.y - v.y);
+	}
+	mul(s)
+	{
+		return new Vecteur2(this.x * s, this.y * s);
+	}
+	div(s)
+	{
+		return new Vecteur2(this.x / s, this.y / s);
+	}
+	length()
+	{
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	normalize()
+	{
+		let len = this.length();
+		return new Vecteur2(this.x / len, this.y / len);
+	}
+	angle()
+	{
+		return Math.atan2(this.y, this.x);
+	}
+	rotate(angle)
+	{
+		let cos = Math.cos(angle);
+		let sin = Math.sin(angle);
+		return new Vecteur2(this.x * cos - this.y * sin, this.x * sin + this.y * cos);
+	}
+	static fromAngle(angle)
+	{
+		return new Vecteur2(Math.cos(angle), Math.sin(angle));
+	}
+	static distance(v1, v2)
+	{
+		return v1.sub(v2).length();
+	}
+	distanceTo(v)
+	{
+		return this.sub(v).length();
+	}
+
+	dot(v)
+	{
+		return this.x * v.x + this.y * v.y;
+	}
+	atright(v)
+	{
+		return this.x*-v.y + this.y*v.x;
+	}
+}
+
+class Ligne
+{
+	p1;
+	p2;
+	constructor(p1, p2)
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	direction()
+	{
+		return this.p2.sub(this.p1).normalize();
+	}
+	intersection(line)
+	{
+		let x1 = this.p1.x;
+		let y1 = this.p1.y;
+		let x2 = this.p2.x;
+		let y2 = this.p2.y;
+		let x3 = line.p1.x;
+		let y3 = line.p1.y;
+		let x4 = line.p2.x;
+		let y4 = line.p2.y;
+		let denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+		if (denom == 0) return null;
+		let px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
+		let py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
+		return new Vecteur2(px, py);
+	}
+	toBorder(w, h)
+	{
+		let line1 = new Ligne(new Vecteur2(0, 0), new Vecteur2(w, 0));
+		let line2 = new Ligne(new Vecteur2(w, 0), new Vecteur2(w, h));
+		let line3 = new Ligne(new Vecteur2(w, h), new Vecteur2(0, h));
+		let line4 = new Ligne(new Vecteur2(0, h), new Vecteur2(0, 0));
+		let points = [];
+		let p;
+		p = this.intersection(line1);
+		if (p) points.push(p);
+		p = this.intersection(line2);
+		if (p) points.push(p);
+		p = this.intersection(line3);
+		if (p) points.push(p);
+		p = this.intersection(line4);
+		if (p) points.push(p);
+		if (points.length < 2) return null;
+		points.sort((v1, v2) => { return v1.distanceTo(new Vecteur2(w / 2, h / 2)) - v2.distanceTo(new Vecteur2(w / 2, h / 2)); });
+		return new Ligne(points[0], points[1]);
+	}
+	middle()
+	{
+		return this.p1.add(this.p2).div(2);
+	}
+	pointIn(t)
+	{
+		return this.p1.add(this.p2.sub(this.p1).mul(t));
+	}
+	toHalfLine(w, h, flip=false)
+	{
+		let border_line = this.toBorder(w, h);
+		let dot = this.direction().dot(border_line.direction());
+		if (dot > 0)
+		{
+			if (flip)
+				return new Ligne(this.p2, border_line.p1);
+			else
+				return new Ligne(this.p1, border_line.p2);
+		}
+		else
+		{
+			if (flip)
+				return new Ligne(this.p2, border_line.p2);
+			else
+				return new Ligne(this.p1, border_line.p1);
+		}
+	}
+}
+
+class Angle
+{
+	p1
+	p2;
+	p3;
+
+	constructor(p1, p2, p3)
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+	}
+
+	angle()
+	{
+		let v1 = this.p1.sub(this.p2).normalize();
+		let v2 = this.p3.sub(this.p2).normalize();
+		return Math.acos(v1.dot(v2));
+	}
+
+	bisectrice()
+	{
+		let v1 = this.p1.sub(this.p2).normalize();
+		let v2 = this.p3.sub(this.p2).normalize();
+		let bisect = v1.add(v2).normalize();
+		return bisect;
+	}
+}
+
+class Geometrie
+{
+	SVG_Draw;
+
+	base_group;
+	txt_group;
+
+	base_line_attr = {
+		stroke: "black",
+		fill: "none",
+		"stroke-width": 2,
+		"stroke-linecap": "round",
+		"stroke-linejoin": "round",
+	}
+	base_txt_attr = {
+		stroke: "none",
+		fill: "black",
+		"text-anchor": "middle",
+		"font-family": "Bahnschrift",
+		"font-size": 16,
+	}
+	base_txt_back_attr = {
+		stroke: "white",
+		"stroke-width": 2,
+		fill: "white",
+		"text-anchor": "middle",
+		"font-family": "Bahnschrift",
+		"font-size": 16,
+	}
+	
+	constructor(width, height)
+	{
+		this.SVG_Draw = SVG();
+		this.SVG_Draw.size(width, height);
+		this.SVG_Draw.rect(width,height).attr({fill: "white"});
+		this.base_group = this.SVG_Draw.group();
+		this.txt_group = this.SVG_Draw.group();
+	}
+	
+	GetElement()
+	{
+		return this.SVG_Draw.node
+	}
+
+	
+	AjouterPoint(v,nom, param = {})
+	{
+		let angle = param.hasOwnProperty("angle") ? param.angle : 90;
+		let distance = param.hasOwnProperty("distance") ? param.distance : 15;
+		let offsetX = distance * Math.cos(angle * Math.PI / 180);
+		let offsetY = -distance * Math.sin(angle * Math.PI / 180);
+		let type = param.hasOwnProperty("type") ? param.type : "+";
+		let size = param.hasOwnProperty("size") ? param.size : 10;
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		let attr_sup_text = param.hasOwnProperty("attr-text") ? param["attr-text"] : {};
+		
+		switch (type) {
+			case "+":
+				this.base_group.line(v.x - size / 2,v.y, v.x + size / 2,v.y).attr(this.base_line_attr).attr(attr_sup);
+				this.base_group.line(v.x, v.y - size / 2, v.x, v.y + size / 2).attr(this.base_line_attr).attr(attr_sup);		
+				break;
+			case "x":
+				this.base_group.line(v.x - size / 2,v.y - size / 2, v.x + size / 2,v.y + size / 2).attr(this.base_line_attr).attr(attr_sup);
+				this.base_group.line(v.x - size / 2,v.y + size / 2, v.x + size / 2,v.y - size / 2).attr(this.base_line_attr).attr(attr_sup);		
+				break;
+			case "o":
+				this.base_group.circle(size).attr(this.base_line_attr).attr(attr_sup).center(v.x,v.y);
+				break;
+			default:
+				break;
+		}
+		this.txt_group.text(nom).attr(this.base_txt_attr).attr(attr_sup_text).attr(this.base_txt_back_attr).center(v.x + offsetX, v.y + offsetY);
+		this.txt_group.text(nom).attr(this.base_txt_attr).attr(attr_sup_text).center(v.x + offsetX, v.y + offsetY);
+	}
+
+	AjouterSegment(line, param = {})
+	{
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		this.base_group.line(line.p1.x, line.p1.y, line.p2.x, line.p2.y).attr(this.base_line_attr).attr(attr_sup);
+		let codage = param.hasOwnProperty("codage") ? param.codage : "none";
+		let c_size = param.hasOwnProperty("codage-size") ? param["codage-size"] : 20;
+		let c_color = param.hasOwnProperty("codage-color") ? param["codage-color"] : "#43a047";
+
+		let middle = line.middle();
+		let dir = line.direction();
+		if (codage != "none")
+		{
+			switch(codage)
+			{
+				case "/":
+					let v3 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2));
+					let v4 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2));
+					this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr({stroke: c_color});
+					break;
+				case "//":
+					let offset = dir.mul(c_size / 3);
+					let v5 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2)).add(offset);
+					let v6 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2)).add(offset);
+					this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr({stroke: c_color});
+					let v7 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2)).sub(offset);
+					let v8 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2)).sub(offset);
+					this.base_group.line(v7.x, v7.y, v8.x, v8.y).attr(this.base_line_attr).attr({stroke: c_color});
+					break;
+				case "///":
+					let offset2 = dir.mul(c_size / 2);
+					let v9 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2));
+					let v10 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2));
+					this.base_group.line(v9.x, v9.y, v10.x, v10.y).attr(this.base_line_attr).attr({stroke: c_color});
+					let v11 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2)).add(offset2);
+					let v12 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2)).add(offset2);
+					this.base_group.line(v11.x, v11.y, v12.x, v12.y).attr(this.base_line_attr).attr({stroke: c_color});
+					let v13 = middle.add(dir.rotate(Math.PI / 2).mul(c_size / 2)).sub(offset2);
+					let v14 = middle.add(dir.rotate(-Math.PI / 2).mul(c_size / 2)).sub(offset2);
+					this.base_group.line(v13.x, v13.y, v14.x, v14.y).attr(this.base_line_attr).attr({stroke: c_color});
+					break;
+				case "x":
+					let v15 = middle.add(dir.rotate(Math.PI / 4).mul(c_size / 2));
+					let v16 = middle.add(dir.rotate(-Math.PI / 4).mul(c_size / 2));
+					let v17 = middle.add(dir.rotate(3 * Math.PI / 4).mul(c_size / 2));
+					let v18 = middle.add(dir.rotate(-3 * Math.PI / 4).mul(c_size / 2));
+					this.base_group.line(v15.x, v15.y, v18.x, v18.y).attr(this.base_line_attr).attr({stroke: c_color});
+					this.base_group.line(v17.x, v17.y, v16.x, v16.y).attr(this.base_line_attr).attr({stroke: c_color});
+					break;
+				case "o":
+					this.base_group.circle(c_size).attr(this.base_line_attr).attr({stroke: c_color}).center(middle.x, middle.y);
+					break;
+			}
+		}
+		let depart = param.hasOwnProperty("depart") ? param.depart : false;
+		let d_size = param.hasOwnProperty("depart-size") ? param["depart-size"] : 15;
+		let fin = param.hasOwnProperty("fin") ? param.fin : false;
+		let f_size = param.hasOwnProperty("fin-size") ? param["fin-size"] : 15;
+		if (depart)
+		{
+			let v3 = line.p1.add(dir.rotate(Math.PI / 2).mul(d_size / 2));
+			let v4 = line.p1.add(dir.rotate(-Math.PI / 2).mul(d_size / 2));
+			this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+		if (fin)
+		{
+			let v5 = line.p2.add(dir.rotate(Math.PI / 2).mul(f_size / 2));
+			let v6 = line.p2.add(dir.rotate(-Math.PI / 2).mul(f_size / 2));
+			this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+	}
+
+	AjouterDroite(line, param = {})
+	{
+		let border_line = line.toBorder(this.SVG_Draw.width(), this.SVG_Draw.height());
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		this.base_group.line(border_line.p1.x, border_line.p1.y, border_line.p2.x, border_line.p2.y).attr(this.base_line_attr).attr(attr_sup);
+		
+		let dir = line.direction().rotate(Math.PI / 2);
+		let depart = param.hasOwnProperty("depart") ? param.depart : false;
+		let d_size = param.hasOwnProperty("depart-size") ? param["depart-size"] : 15;
+		let fin = param.hasOwnProperty("fin") ? param.fin : false;
+		let f_size = param.hasOwnProperty("fin-size") ? param["fin-size"] : 15;
+		if (depart)
+		{
+			let v3 = line.p1.add(dir.mul(d_size / 2));
+			let v4 = line.p1.add(dir.mul(-d_size / 2));
+			this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+		if (fin)
+		{
+			let v5 = line.p2.add(dir.mul(f_size / 2));
+			let v6 = line.p2.add(dir.mul(-f_size / 2));
+			this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+
+		if (param.hasOwnProperty("nom"))
+		{
+			let pos_target =  param.hasOwnProperty("nom-position") ? param["nom-position"] : 0;
+			let dist =  param.hasOwnProperty("nom-distance") ? param["nom-distance"] : 15;
+			let marge =  param.hasOwnProperty("nom-margebord") ? param["nom-margebord"] : 15;
+			let txt_position;
+			let pos = border_line.p1.distanceTo(line.p1) < border_line.p1.distanceTo(line.p2) ? pos_target: (pos_target + 1) % 2;
+			if (pos == 0)
+			{
+				let dot2 = dir.dot(border_line.p1.sub(new Vecteur2(this.SVG_Draw.width() / 2, this.SVG_Draw.height() / 2)));
+				if (dot2 < 0)
+					txt_position = border_line.p1.add(dir.mul(dist));
+				else
+					txt_position = border_line.p1.add(dir.mul(-dist));
+			}
+			else if (pos == 1)
+			{
+
+				let dot2 = dir.dot(line.p2.sub(new Vecteur2(this.SVG_Draw.width() / 2, this.SVG_Draw.height() / 2)));
+				if (dot2 > 0)
+					txt_position = border_line.p2.add(dir.mul(dist));
+				else
+					txt_position = border_line.p2.add(dir.mul(-dist));
+			}
+			let count = 20;
+			while(count > 0 && (txt_position.x < marge || txt_position.x > this.SVG_Draw.width() - marge || txt_position.y < marge || txt_position.y > this.SVG_Draw.height() - marge))
+			{
+				txt_position = txt_position.add(border_line.direction().mul(pos == 0 ? 5	: -5));
+				count--;
+			}
+			this.AjoutText(txt_position, param.nom, param.hasOwnProperty("nom-attr") ? param["nom-attr"] : {});
+		}
+
+		return border_line;
+	}
+
+	AjouterDemiDroite(line, param = {})
+	{
+		let border_line = line.toBorder(this.SVG_Draw.width(), this.SVG_Draw.height());
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+
+		let dot = line.direction().dot(border_line.direction());
+		if (dot > 0)
+			this.base_group.line(line.p1.x, line.p1.y, border_line.p2.x, border_line.p2.y).attr(this.base_line_attr).attr(attr_sup);
+		else
+			this.base_group.line(line.p1.x, line.p1.y, border_line.p1.x, border_line.p1.y).attr(this.base_line_attr).attr(attr_sup);
+		
+		let depart = param.hasOwnProperty("depart") ? param.depart : false;
+		let d_size = param.hasOwnProperty("depart-size") ? param["depart-size"] : 15;
+		let fin = param.hasOwnProperty("fin") ? param.fin : false;
+		let f_size = param.hasOwnProperty("fin-size") ? param["fin-size"] : 15;
+		let direction = line.direction();
+		if (depart)
+		{
+			let v3 = line.p1.add(direction.rotate(Math.PI / 2).mul(d_size / 2));
+			let v4 = line.p1.add(direction.rotate(-Math.PI / 2).mul(d_size / 2));
+			let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+			this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+		if (fin)
+		{
+			let v5 = line.p2.add(direction.rotate(Math.PI / 2).mul(f_size / 2));
+			let v6 = line.p2.add(direction.rotate(-Math.PI / 2).mul(f_size / 2));
+			this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr(attr_sup);
+		}
+
+		if (dot > 0)
+			return new Ligne(line.p1, border_line.p2);
+		else
+			return new Ligne(line.p1, border_line.p1);
+	}
+
+	AjoutText(v, txt, param = {})
+	{
+		let offsetX = param.hasOwnProperty("offsetX") ? param.offsetX : 0;
+		let offsetY = param.hasOwnProperty("offsetY") ? param.offsetY : 0;
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		this.txt_group.text(txt).attr(this.base_txt_attr).attr(attr_sup).attr(this.base_txt_back_attr).center(v.x + offsetX, v.y + offsetY);
+		this.txt_group.text(txt).attr(this.base_txt_attr).attr(attr_sup).center(v.x + offsetX, v.y + offsetY);
+	}
+
+	AjoutPolygone(points, param = {})
+	{
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		let points_svg = points.map(p => `${p.x},${p.y}`).join(" ");
+		this.base_group.polygon(points_svg).attr(this.base_line_attr).attr(attr_sup);
+	}
+
+	AjoutAngle(angle, dir = 0, param = {})
+	{
+		let bisect = angle.bisectrice();
+		let radius = param.hasOwnProperty("radius") ? param.radius : 30;
+		let attr_sup = param.hasOwnProperty("attr") ? param.attr : {};
+		
+		let p_start = angle.p2.add(angle.p1.sub(angle.p2).normalize().mul(radius));
+		let p_end = angle.p2.add(angle.p3.sub(angle.p2).normalize().mul(radius));
+		let arc = "M " + p_start.x + " " + p_start.y + " ";
+
+		arc += "A " + radius + " " + radius + " 0 "
+
+		let right = angle.p1.sub(angle.p2).atright(angle.p3.sub(angle.p2))
+		if(right >= 0 && dir == 0)
+			arc += "0 0 "
+		else if(right >= 0 && dir == 1)
+			arc += "1 1 "
+		else if(right < 0 && dir == 0)
+			arc += "0 1 "
+		else if(right < 0 && dir == 1)
+			arc += "1 0 "
+		arc += p_end.x + " " + p_end.y;
+		this.base_group.path(arc).attr(this.base_line_attr).attr(attr_sup)
+
+
+		let codage = param.hasOwnProperty("codage") ? param.codage : "none";
+		let c_size = param.hasOwnProperty("codage-size") ? param["codage-size"] : 20;
+		let c_color = param.hasOwnProperty("codage-color") ? param["codage-color"] : "#43a047";
+
+		let middle = angle.p2.add(bisect.mul(radius * (dir == 0 ? 1 : -1)));
+		if (codage != "none")
+		{
+			switch(codage)
+			{
+				case "/":
+					{
+						let v1 = middle.add(bisect.mul(c_size / 2));
+						let v2 = middle.add(bisect.mul(-c_size / 2));
+						this.base_group.line(v1.x, v1.y, v2.x, v2.y).attr(this.base_line_attr).attr({stroke: c_color});
+					}
+					break;
+				case "//":
+					{
+						let a1 = new Angle(angle.p1, angle.p2, angle.p2.add(bisect.mul(dir == 0 ? 200 : -200)));
+						let a2 = new Angle(angle.p3, angle.p2, angle.p2.add(bisect.mul(dir == 0 ? 200 : -200)));
+						let b1 = a1.bisectrice();
+						let b2 = a2.bisectrice();
+						let m1 = angle.p2.add(b1.mul(radius * (dir == 0 ? 1 : -1)));
+						let m2 = angle.p2.add(b2.mul(radius * (dir == 0 ? 1 : -1)));
+						let v3 = m1.add(b1.mul(c_size / 2));
+						let v4 = m1.add(b1.mul(-c_size / 2));
+						let v5 = m2.add(b2.mul(c_size / 2));
+						let v6 = m2.add(b2.mul(-c_size / 2));
+						this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr({stroke: c_color});
+						this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr({stroke: c_color});
+					}
+					break;
+				case "///":
+					{
+						let a1 = new Angle(angle.p1, angle.p2, angle.p2.add(bisect.mul(dir == 0 ? 200 : -200)));
+						let a2 = new Angle(angle.p3, angle.p2, angle.p2.add(bisect.mul(dir == 0 ? 200 : -200)));
+						let b1 = a1.bisectrice();
+						let b2 = a2.bisectrice();
+						let m1 = angle.p2.add(b1.mul(radius * (dir == 0 ? 1 : -1)));
+						let m2 = angle.p2.add(b2.mul(radius * (dir == 0 ? 1 : -1)));
+						let v1 = middle.add(bisect.mul(c_size / 2));
+						let v2 = middle.add(bisect.mul(-c_size / 2));
+						let v3 = m1.add(b1.mul(c_size / 2));
+						let v4 = m1.add(b1.mul(-c_size / 2));
+						let v5 = m2.add(b2.mul(c_size / 2));
+						let v6 = m2.add(b2.mul(-c_size / 2));
+						this.base_group.line(v1.x, v1.y, v2.x, v2.y).attr(this.base_line_attr).attr({stroke: c_color});
+						this.base_group.line(v3.x, v3.y, v4.x, v4.y).attr(this.base_line_attr).attr({stroke: c_color});
+						this.base_group.line(v5.x, v5.y, v6.x, v6.y).attr(this.base_line_attr).attr({stroke: c_color});;
+					}
+					break;
+				case "x":
+					let v15 = middle.add(bisect.rotate(Math.PI / 4).mul(c_size / 2));
+					let v16 = middle.add(bisect.rotate(-Math.PI / 4).mul(c_size / 2));
+					let v17 = middle.add(bisect.rotate(3 * Math.PI / 4).mul(c_size / 2));
+					let v18 = middle.add(bisect.rotate(-3 * Math.PI / 4).mul(c_size / 2));
+					this.base_group.line(v15.x, v15.y, v18.x, v18.y).attr(this.base_line_attr).attr({stroke: c_color});
+					this.base_group.line(v17.x, v17.y, v16.x, v16.y).attr(this.base_line_attr).attr({stroke: c_color});
+					break;
+				case "o":
+					this.base_group.circle(c_size / 2).attr(this.base_line_attr).attr({stroke: c_color}).center(middle.x, middle.y);
+					break;
+			}
+		}
+	}
+
+
+}
