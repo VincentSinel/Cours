7c470e42e855909e89e9ff66fe45a9d315d28437
VSProgramme <vincentsinel@gmail.com>
2026-05-06 11:35:40 +0200

    Ajout images cour

--- Cours/Liste_Chapitre.js
+++ Cours/Liste_Chapitre.js
@@ -3000,6 +3000,7 @@ var sixieme_saintexupery = [
 			pdf_trou:  "/Cours/6eme/SaintExupery/Chapitre_11_3/Documents/Chapitre 11 - Partie 3 - Volumes (A trou).pdf",
 			actx: 		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 11 - Activité d'introduction.docx",
 			act:  		 "/Cours/6eme/SaintExupery/Activités d'introductions/Chapitre 11 - Activité d'introduction.pdf",
+			img: 			 "/Cours/6eme/SaintExupery/Chapitre_11_3/Documents/Chapitre 11 - Partie 3 - Images.pdf",
 		},
 		ready : true,
 		page : "SaintExupery/Chapitre_11_3/Cours.html",
